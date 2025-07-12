1fa80ed6af41d2365c7766e3a025bb9f36ca59f2
David Buchanan <d@vidbuchanan.co.uk>
2025-07-12 03:39:58 +0100

    pubkey formatting, signing

--- go.mod
+++ go.mod
@@ -10,7 +10,12 @@ require (
 )
 
 require (
+	github.com/bluesky-social/indigo v0.0.0-20250709210541-ef43ad32f9ac // indirect
 	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
 	github.com/manifoldco/promptui v0.9.0 // indirect
-	golang.org/x/sys v0.0.0-20181122145206-62eef0e2fa9b // indirect
+	github.com/mr-tron/base58 v1.2.0 // indirect
+	gitlab.com/yawning/secp256k1-voi v0.0.0-20230925100816-f2616030848b // indirect
+	gitlab.com/yawning/tuplehash v0.0.0-20230713102510-df83abbf9a02 // indirect
+	golang.org/x/crypto v0.21.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
 )
--- yubiplc.go
+++ yubiplc.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"context"
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/sha256"
+	"encoding/asn1"
+	"encoding/base64"
 	"errors"
 	"fmt"
 	"log"
+	"math/big"
 	"os"
 	"strings"
 
+	indigo "github.com/bluesky-social/indigo/atproto/crypto"
 	"github.com/go-piv/piv-go/v2/piv"
 	"github.com/manifoldco/promptui"
 	"github.com/urfave/cli/v3"
@@ -19,7 +27,7 @@ func main() {
 		Commands: []*cli.Command{
 			{
 				Name:   "init",
-				Usage:  "generate a new private key on the yubikey",
+				Usage:  "generate a new NIST-P256 private key on the yubikey, in slot 9C",
 				Action: doInit,
 				Flags: []cli.Flag{
 					&cli.BoolFlag{
@@ -30,12 +38,12 @@ func main() {
 			},
 			{
 				Name:   "pubkey",
-				Usage:  "print the public key to stdout, in did:key format",
+				Usage:  "print the corresponding public key to stdout, in did:key format",
 				Action: doPubkey,
 			},
 			{
 				Name:   "sign",
-				Usage:  "sign a did:plc operation (reads JSON from stdin)",
+				Usage:  "sign a did:plc operation (reads and writes JSON on stdio)",
 				Action: doSign,
 			},
 		},
@@ -69,6 +77,30 @@ func findYubikey() (*piv.YubiKey, error) {
 	return yk, nil
 }
 
+func pubkeyToDidKey(pubkey crypto.PublicKey) (string, error) {
+	ecdsa_pubkey, ok := pubkey.(*ecdsa.PublicKey)
+	if !ok {
+		return "", errors.New("P256 public key expected")
+	}
+
+	if ecdsa_pubkey.Curve != elliptic.P256() {
+		return "", errors.New("P256 public key expected")
+	}
+
+	// these APIs are weird... I'm not doing ECDH but elliptic.Marshal is deprecated
+	ecdh_pubkey, err := ecdsa_pubkey.ECDH()
+	if err != nil {
+		return "", err
+	}
+
+	indigo_pubkey, err := indigo.ParsePublicUncompressedBytesP256(ecdh_pubkey.Bytes())
+	if err != nil {
+		return "", err
+	}
+
+	return indigo_pubkey.DIDKey(), nil
+}
+
 func doInit(ctx context.Context, cmd *cli.Command) error {
 	yk, err := findYubikey()
 	if err != nil {
@@ -92,27 +124,94 @@ func doInit(ctx context.Context, cmd *cli.Command) error {
 		}
 	}
 
-	// Generate a private key on the YubiKey.
 	key := piv.Key{
 		Algorithm:   piv.AlgorithmEC256,
 		PINPolicy:   piv.PINPolicyAlways,
 		TouchPolicy: piv.TouchPolicyAlways,
 	}
-	_, err = yk.GenerateKey(piv.DefaultManagementKey, piv.SlotSignature, key)
+	pubkey, err := yk.GenerateKey(piv.DefaultManagementKey, piv.SlotSignature, key)
 	if err != nil {
 		return err
 	}
 
-	fmt.Println("success") // TODO: print pubkey
+	didkey, err := pubkeyToDidKey(pubkey)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(didkey)
 	return nil
 }
 
 func doPubkey(ctx context.Context, cmd *cli.Command) error {
-	// TODO
+	yk, err := findYubikey()
+	if err != nil {
+		return err
+	}
+
+	cert, err := yk.Attest(piv.SlotSignature)
+	if err != nil {
+		return err
+	}
+
+	didkey, err := pubkeyToDidKey(cert.PublicKey)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(didkey)
 	return nil
 }
 
+func asn1SigToCompact(asn1sig []byte) ([]byte, error) {
+	var rs struct {
+		R, S *big.Int
+	}
+	_, err := asn1.Unmarshal(asn1sig, &rs)
+	if err != nil {
+		return nil, err
+	}
+
+	var compact [64]byte
+	rs.R.FillBytes(compact[:32])
+	rs.S.FillBytes(compact[32:])
+
+	return compact[:], nil
+}
+
 func doSign(ctx context.Context, cmd *cli.Command) error {
-	// TODO
+	yk, err := findYubikey()
+	if err != nil {
+		return err
+	}
+
+	// pull out the pubkey
+	cert, err := yk.Attest(piv.SlotSignature)
+	if err != nil {
+		return err
+	}
+
+	auth := piv.KeyAuth{PIN: piv.DefaultPIN} // TODO: option to prompt for pin interactively
+	privkey, err := yk.PrivateKey(piv.SlotSignature, cert.PublicKey, auth)
+	if err != nil {
+		return err
+	}
+
+	ecdsa_privkey := privkey.(*piv.ECDSAPrivateKey)
+
+	msg := "hello"
+	digest := sha256.Sum256([]byte(msg))
+	fmt.Fprintln(os.Stderr, "please boop your yubikey now")
+	asn1sig, err := ecdsa_privkey.Sign(nil, digest[:], crypto.SHA256)
+	if err != nil {
+		return err
+	}
+
+	compactsig, err := asn1SigToCompact(asn1sig)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(base64.RawURLEncoding.EncodeToString(compactsig))
 	return nil
 }
